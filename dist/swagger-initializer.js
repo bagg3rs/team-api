f9d1a453d477242b245c28f18b37106f4c195f54
Richard Baguley <20583399+bagg3rs@users.noreply.github.com>
2023-08-01 23:07:26 +0100

    🚑 fix file and url lol
    Signed-off-by: Richard Baguley <20583399+bagg3rs@users.noreply.github.com>

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://bagg3rs.github.io/team-api/v2/swagger.json",
+    url: "swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
